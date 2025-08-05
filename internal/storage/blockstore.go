cd63f0643893ab3daea2b8d6e81fe32b80b9193f
Yash <yashsaini18166@gmail.com>
2025-08-05 15:12:20 +0530

    Update Go module, add generated protobuf files, implement BlockStore, and add tests

---
+++ internal/storage/blockstore.go
@@ -0,0 +1,60 @@
+package storage
+
+import (
+	"log"
+
+	"github.com/dgraph-io/badger/v4"
+)
+
+type BlockStore struct {
+	db *badger.DB
+}
+
+func New(path string) (*BlockStore, error) {
+	opts := badger.DefaultOptions(path)
+	db, err := badger.Open(opts)
+
+	if err != nil {
+		return nil, err
+	}
+	return &BlockStore{
+		db: db,
+	}, nil
+}
+
+func (bs *BlockStore) Put(data []byte) (string, error) {
+	cid := Hash(data)
+	return cid, bs.db.Update(func(txn *badger.Txn) error {
+		return txn.Set([]byte(cid), data)
+	})
+}
+
+func (bs *BlockStore) Get(cid string) ([]byte, error) {
+	var blockData []byte
+	err := bs.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(cid))
+		if err != nil {
+			return err
+		}
+		return item.Value(func(val []byte) error {
+			blockData = append([]byte{}, val...)
+			return nil
+		})
+	})
+	return blockData, err
+}
+
+func (bs *BlockStore) Has(cid string) (bool, error) {
+	err := bs.db.View(func(txn *badger.Txn) error {
+		_, err := txn.Get([]byte(cid))
+		return err
+	})
+	return err == nil, err
+}
+
+func (bs *BlockStore) Close() {
+	err := bs.db.Close()
+	if err != nil {
+		log.Println("Error Closing the BadgerDB", err)
+	}
+}
---
+++ internal/storage/blockstore_test.go
@@ -0,0 +1,35 @@
+package storage
+
+import (
+	"os"
+	"testing"
+)
+
+func TestBlockStore_PutGetHas(t *testing.T) {
+	_ = os.RemoveAll("./tmpdb")
+	store, err := New("./tmpdb")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer store.Close()
+
+	data := []byte("Hello World Buddy!!!")
+
+	cid, err := store.Put(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ok, _ := store.Has(cid)
+	if !ok {
+		t.Fatal("Block not found after Put")
+	}
+
+	got, err := store.Get(cid)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(got) != string(data) {
+		t.Fatalf("expected %s, got %s", data, got)
+	}
+
+}
